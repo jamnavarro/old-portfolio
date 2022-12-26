7c2f8dd1244134cca273645d2469d0d924e10733
jamnavarro <jamsnavarro1@gmail.com>
2022-12-26 00:58:14 -0600

    change navigation bar to responsive on all pages

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+// 
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+
+// reveals dropdown navigation menu
+var tglbtn = document.getElementById("nav-toggle");
+var navlst = document.getElementById("nav-list")
+
+tglbtn.addEventListener('click', () => {
+  navlst.classList.toggle("active");
+});
