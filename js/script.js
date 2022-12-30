efdf09325c3ca06c33373ae480bfc8e5035c834b
jamnavarro <jamsnavarro1@gmail.com>
2022-12-29 23:48:36 -0600

    edits to js file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// 
+// smooth scrolls to projects section
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
